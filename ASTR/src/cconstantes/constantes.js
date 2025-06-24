37713fe8927e535f1cd4c46af37230d5a92b83cd
parinohernan <parinohernan@gmail.com>
2025-06-23 23:51:51 -0300

    actualizo vercion

--- ASTR/src/cconstantes/constantes.js
+++ ASTR/src/cconstantes/constantes.js
@@ -1,7 +1,7 @@
 
 const empresa = "janus314 - HernyDev";
 const producto = "Osvi";
-const version = "v 1.3.20250622";
+const version = "v 1.3.20250623";
 const whatsapp = "+54 2923570231";
 const mail = "parinohernan@gmail.com";
 module.exports = {empresa, producto, version, whatsapp, mail}
